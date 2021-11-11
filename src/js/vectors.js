a06ff4d563df69f9f3a113e975c686134d3a1234
p.pozhidaev <p.pozhidaev@msaip.ru>
2021-11-11 21:34:30 +1000

    Разделение мат.модели от логики игры

---
+++ src/js/vectors.js
@@ -0,0 +1,28 @@
+function vAdd(v1, v2) {
+    return [v1[0] + v2[0], v1[1] + v2[1]];
+}
+function vAddScalar(v, s) {
+    return [v[0] + s, v[1] + s];
+}
+function vSub(v1, v2) {
+    return [v1[0] - v2[0], v1[1] - v2[1]];
+}
+function vMultScalar(v, s) {
+    return [v[0] * s, v[1] * s];
+}
+function vMultVectorScalar(v1, v2) {
+    return v1[0] * v2[0] + v1[1] * v2[1];
+}
+function angleBetweenVectors(v1, v2) {
+    return Math.acos(vMultVectorScalar(v1, v2));
+}
+function vMult(v1, v2) {
+
+}
+function vLength(v) {
+    return Math.sqrt((v[0] * v[0]) + (v[1] * v[1]));
+}
+function vNormal(v) {
+    let l = vLength(v);
+    return [v[0] / l, v[1] / l];
+}
